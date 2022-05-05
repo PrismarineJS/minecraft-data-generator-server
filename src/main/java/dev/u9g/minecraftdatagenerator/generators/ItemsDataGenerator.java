575b0205578b77b1c3d5de1bf098e9302a33ff32
u9g <43508353+u9g@users.noreply.github.com>
2022-05-05 19:49:05 -0400

    remove durability prop on items

--- src/main/java/dev/u9g/minecraftdatagenerator/generators/ItemsDataGenerator.java
+++ src/main/java/dev/u9g/minecraftdatagenerator/generators/ItemsDataGenerator.java
@@ -70,7 +70,6 @@ public class ItemsDataGenerator implements IDataGenerator {
             itemDesc.add("repairWith", fixedWithArray);
 
             int maxDurability = item.getMaxDamage();
-            itemDesc.addProperty("durability", maxDurability);
             itemDesc.addProperty("maxDurability", maxDurability);
         }
         return itemDesc;
