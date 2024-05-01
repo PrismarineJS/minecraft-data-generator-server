a46cb8fc22e0047db2ae11e3e2cb10ff46234827
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-05-01 21:06:41 +0200

    More improvements

--- 1.14/src/main/java/dev/u9g/minecraftdatagenerator/generators/SoundsDataGenerator.java
+++ 1.14/src/main/java/dev/u9g/minecraftdatagenerator/generators/SoundsDataGenerator.java
@@ -4,14 +4,15 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import dev.u9g.minecraftdatagenerator.mixin.SoundAccessor;
 import net.minecraft.client.sound.Sound;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.registry.Registry;
 
 public class SoundsDataGenerator implements IDataGenerator {
-    public static JsonObject generateSound(Registry<Sound> registry, Sound soundEvent) {
+    public static JsonObject generateSound(SoundEvent soundEvent) {
         JsonObject soundDesc = new JsonObject();
 
-        soundDesc.addProperty("id", registry.getRawId(soundEvent));
-        soundDesc.addProperty("name", ((SoundAccessor) soundEvent).id().getPath());
+        soundDesc.addProperty("id", Registry.SOUND_EVENT.getRawId(soundEvent));
+        soundDesc.addProperty("name", soundEvent.getId().getPath());
 
         return soundDesc;
     }
@@ -24,8 +25,7 @@ public class SoundsDataGenerator implements IDataGenerator {
     @Override
     public JsonArray generateDataJson() {
         JsonArray resultsArray = new JsonArray();
-        Registry<Sound> soundEventRegistry = Registry.SOUND_EVENT;
-        soundEventRegistry.forEach(sound -> resultsArray.add(generateSound(soundEventRegistry, sound)));
+        Registry.SOUND_EVENT.forEach(sound -> resultsArray.add(generateSound(sound)));
         return resultsArray;
     }
 }
